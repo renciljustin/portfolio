a1f647e8d162b5acbf9bc1564f35d255fcfd69bf
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2025-10-03 17:49:36 +0800

    chore: remove / prefix on image path

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} unoptimized={true} />
+        <Image className="dark:invert" src="next.svg" alt="Next.js logo" width={180} height={38} unoptimized={true} />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{' '}
@@ -24,7 +24,7 @@ export default function Home() {
             rel="noopener noreferrer">
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -47,7 +47,7 @@ export default function Home() {
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer">
-          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} unoptimized={true} />
+          <Image aria-hidden src="file.svg" alt="File icon" width={16} height={16} unoptimized={true} />
           Learn
         </a>
         <a
@@ -55,7 +55,7 @@ export default function Home() {
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer">
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} unoptimized={true} />
+          <Image aria-hidden src="window.svg" alt="Window icon" width={16} height={16} unoptimized={true} />
           Examples
         </a>
         <a
@@ -63,7 +63,7 @@ export default function Home() {
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer">
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} unoptimized={true} />
+          <Image aria-hidden src="globe.svg" alt="Globe icon" width={16} height={16} unoptimized={true} />
           Go to nextjs.org →
         </a>
       </footer>
